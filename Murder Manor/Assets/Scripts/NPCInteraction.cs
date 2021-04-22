97b88fd8a0fca225774402929b93eb83dc89a0ba
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-04-22 13:53:04 +0200

    Doors can now open!
    Pog!
    
    Co-Authored-By: TimKuijpers2002 <57141910+TimKuijpers2002@users.noreply.github.com>

---
+++ Murder Manor/Assets/Scripts/NPCInteraction.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+using UnityEngine.Windows.Speech;
+using System;
+using System.Linq;
+using UnityEngine.UI;
+
+public class NPCInteraction : MonoBehaviour
+{
+
+    private string chefFile = @"Assets/Resources/ChefScenario1.txt";
+    private string chefFile2 = @"Assets/Resources/ChefScenario2.txt";
+    private string gardenerFile = @"Assets/Resources/GardenerScenario1.txt";
+    private string gardenerFile2 = @"Assets/Resources/GardenerScenario2.txt";
+    private string mechanicFile = @"Assets/Resources/MechanicScenario1.txt";
+    private string mechanicFile2 = @"Assets/Resources/MechanicScenario2.txt";
+
+    private int scenarioNumber = 1;
+
+
+    public Text screenText;
+
+    private string npcType;
+
+    private float textDuration = 6f;
+
+    private KeywordRecognizer keywordRecognizer;
+
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("Murder info", Help);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Chef")
+        {
+            npcType = "Chef";
+        }
+        else if (other.tag == "Gardener")
+        {
+            npcType = "Gardener";
+        }
+        else if (other.tag == "Mechanic")
+        {
+            npcType = "Mechanic";
+        }
+    }
+
+    private void Help()
+    {
+        if (npcType == "Chef" && scenarioNumber == 1)
+        {
+            StartCoroutine("ReadChef");
+        }
+        else if (npcType == "Chef" && scenarioNumber == 2)
+        {
+            StartCoroutine("ReadChef2");
+        }
+        else if (npcType == "Gardener" && scenarioNumber == 1)
+        {
+            StartCoroutine("ReadGardener");
+        }
+        else if (npcType == "Gardener" && scenarioNumber == 2)
+        {
+            StartCoroutine("ReadGardener2");
+        }
+        else if (npcType == "Mechanic" && scenarioNumber == 1)
+        {
+            StartCoroutine("ReadMechanic");
+        }
+        else if (npcType == "Mechanic" && scenarioNumber == 2)
+        {
+            StartCoroutine("ReadMechanic2");
+        }
+    }
+
+    private IEnumerator ReadChef()
+    {
+        using (StreamReader sr = new StreamReader(chefFile))
+        {
+            screenText.text = sr.ReadToEnd();
+        }
+        yield return new WaitForSeconds(textDuration);
+
+        screenText.text = "";
+    }
+
+    private IEnumerator ReadChef2()
+    {
+        using (StreamReader sr = new StreamReader(chefFile2))
+        {
+            screenText.text = sr.ReadToEnd();
+        }
+        yield return new WaitForSeconds(textDuration);
+
+        screenText.text = "";
+    }
+
+    private IEnumerator ReadGardener()
+    {
+        using (StreamReader sr = new StreamReader(gardenerFile))
+        {
+            screenText.color = Color.green;
+            screenText.text = sr.ReadToEnd();
+        }
+        yield return new WaitForSeconds(textDuration);
+
+        screenText.text = "";
+        screenText.color = Color.black;
+    }
+
+    private IEnumerator ReadGardener2()
+    {
+        using (StreamReader sr = new StreamReader(gardenerFile2))
+        {
+            screenText.color = Color.green;
+            screenText.text = sr.ReadToEnd();
+        }
+        yield return new WaitForSeconds(textDuration);
+
+        screenText.text = "";
+        screenText.color = Color.black;
+    }
+
+    private IEnumerator ReadMechanic()
+    {
+        using (StreamReader sr = new StreamReader(mechanicFile))
+        {
+            screenText.color = Color.red;
+            screenText.text = sr.ReadToEnd();
+        }
+        yield return new WaitForSeconds(textDuration);
+
+        screenText.text = "";
+        screenText.color = Color.black;
+    }
+
+    private IEnumerator ReadMechanic2()
+    {
+        using (StreamReader sr = new StreamReader(mechanicFile2))
+        {
+            screenText.color = Color.red;
+            screenText.text = sr.ReadToEnd();
+        }
+        yield return new WaitForSeconds(textDuration);
+
+        screenText.text = "";
+        screenText.color = Color.black;
+    }
+}
---
+++ Murder Manor/Assets/Scripts/ScriptSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class ScriptSwitcher : MonoBehaviour
+{
+
+    private NavMeshAgent npcAgent;
+    private Rigidbody npcBody;
+    public GameObject PlayerInteractor;
+
+
+    void OnEnable()
+    {
+        npcAgent = GetComponent<NavMeshAgent>();
+        npcBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<NPCMovement>().enabled = false;
+            npcAgent.isStopped = true;
+            npcBody.constraints = RigidbodyConstraints.FreezePosition;
+            PlayerInteractor.GetComponent<NPCInteraction>().enabled = true;
+            transform.LookAt(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            npcBody.constraints = RigidbodyConstraints.None;
+            npcAgent.isStopped = false;
+            PlayerInteractor.GetComponent<NPCInteraction>().enabled = false;
+            GetComponent<NPCMovement>().enabled = true;
+        }
+    }
+}
