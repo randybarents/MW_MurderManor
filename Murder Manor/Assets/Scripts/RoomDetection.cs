172ee001b4d1b170721d8ddb5ee014512bbd26f4
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2021-05-20 15:03:58 +0200

    Added 3 hitboxes

---
+++ Murder Manor/Assets/Scripts/RoomDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDetection : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player entered "+ transform.parent.name);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player exited " + transform.parent.name);
+        }
+    }
+}
